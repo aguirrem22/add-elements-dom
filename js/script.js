319b0ad1104e4db54d6f855acb8fa9362bae7ef5
aguirrem22 <104278842+aguirrem22@users.noreply.github.com>
2025-11-05 15:31:04 +0100

    Actualizacion Challenge 2 SPRINT 3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
  // Aquí tu código
+const boton = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+boton.addEventListener('click', function () {
+  const nombre = prompt('Introduce tu nombre:');
+  const ciudad = prompt('Introduce tu ciudad:');
+  const edad = prompt('Introduce tu edad:');
+  const profesion = prompt('Introduce tu profesión:');
+
+  const nuevoElemento = document.createElement('li');
+  nuevoElemento.classList.add('card');
+  nuevoElemento.innerHTML = `
+        <h3><strong>Nombre:</strong> ${nombre}</h3>
+        <p><strong>Ciudad:</strong> ${ciudad}</p>
+        <p><strong>Edad:</strong> ${edad}</p>
+        <p><strong>Profesión:</strong> ${profesion}</p>
+    `;
+    lista.appendChild(nuevoElemento);
+});
